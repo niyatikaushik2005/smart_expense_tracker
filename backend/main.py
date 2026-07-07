1eaa71d6d379ef16eb4ee67c79d764ddd61eb853
niyatikaushik2005 <niyatikaushik3111@gmail.com>
2026-07-07 23:50:03 +0530

    live link added

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "*",
+        "https://smart-expense-tracker-lxtjsnakn-niyati-kaushiks-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
